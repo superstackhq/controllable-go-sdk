84b44bdcbeeef2a890d52d416a779bf4024779ae
Somsubhra <somsubhra.bairi@gmail.com>
2022-12-18 19:23:49 +0530

    added method signatures for property crud in the client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,1 +1,40 @@
 package client
+
+import (
+	"context"
+	"time"
+)
+
+type ControllableClient struct {
+	Config     *ControllableClientConfig
+	HTTPClient *ControllableHTTPClient
+}
+
+type ControllableClientConfig struct {
+	ServerEndpoint string
+	Environment    string
+	ClientTimeout  time.Duration
+}
+
+func NewControllableClient(config *ControllableClientConfig) *ControllableClient {
+	return &ControllableClient{
+		Config:     config,
+		HTTPClient: NewControllableHTTPClient(config.ServerEndpoint, config.ClientTimeout),
+	}
+}
+
+func (c *ControllableClient) CreatePropertyValue(ctx context.Context, propertyReferenceValuePairs *PropertyReferenceValuePairs) (*ExecutionResponse, error) {
+	return nil, nil
+}
+
+func (c *ControllableClient) UpdatePropertyValue(ctx context.Context, propertyReferenceValuePairs *PropertyReferenceValuePairs) (*ExecutionResponse, error) {
+	return nil, nil
+}
+
+func (c *ControllableClient) DeletePropertyValue(ctx context.Context, propertyReferenceValuePairs *PropertyReferenceValuePairs) (*ExecutionResponse, error) {
+	return nil, nil
+}
+
+func (c *ControllableClient) ReadPropertyValue(ctx context.Context, readPropertyRequests *ReadPropertyRequests) (*ExecutionResponse, error) {
+	return nil, nil
+}
